9d475b878c766e1bffbeb8fbc3de17082bce904b
Miksiu <miksiu120@gmail.com>
2024-03-22 20:16:02 +0100

    repairing ability icons

--- src/components/elementsAbilities/AbilityCard.tsx
+++ src/components/elementsAbilities/AbilityCard.tsx
@@ -18,7 +18,7 @@ function Card({ name, image, text }: Prop) {
 			data-aos-once='true'
 			onClick={showHeight}
 			className={`abilities__box__card abilities__box__card__${name}`}>
-			<img src={`icon of ${name}`} />
+			<img src={image} alt={`icon of ${name}`} />
 			<h3>{text}</h3>
 		</div>
 	)
